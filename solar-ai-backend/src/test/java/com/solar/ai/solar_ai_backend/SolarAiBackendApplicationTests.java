45ad6b98a770ee8c6d0e1f104a4983d983a8a6c4
nathnael-desta <destanathnaelataro@gmail.com>
2025-11-03 11:49:37 +0300

    initial commit

---
+++ solar-ai-backend/src/test/java/com/solar/ai/solar_ai_backend/SolarAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solar.ai.solar_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
